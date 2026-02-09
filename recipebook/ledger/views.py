1271be84756cb4971872fba05ced0000674e99d3
Zeke_ZB <zekiel.villasurda@gmail.com>
2026-02-09 10:51:15 +0800

    Create Ledger App URLs

--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -7,4 +7,16 @@ from django.views.generic import TemplateView
 def index(request):
     return HttpResponse("Hello, world. You're at the ledger index.")
 
+def recipe_list(request):
+    return HttpResponse("This is the recipe list view.")
+
+def recipe_1(request):
+    return HttpResponse("This is the recipe 1 view.")
+
+def recipe_2(request):
+    return HttpResponse("This is the recipe 2 view.")
+
+# class RecipeListView(TemplateView):
+#     template_name = 'ledger/recipe_list.html'
+
 
