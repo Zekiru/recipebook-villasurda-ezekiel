2c9b7a613fe122fe443606bfffd052e0bbaef8b8
Zeke_ZB <zekiel.villasurda@gmail.com>
2026-02-09 23:16:46 +0800

    Template Styling & Code Polishing

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import index, RecipeListView, Recipe1View, Recipe2View
+from .views import PATH, index, RecipeListView, RecipeView
 
 urlpatterns = [
     path('', index, name='index'),
     path('recipes/list/', RecipeListView.as_view(), name='recipe-list'),
-    path('recipe/1/', Recipe1View.as_view(), name='recipe-1'),
-    path('recipe/2/', Recipe2View.as_view(), name='recipe-2'),
+    path('recipe/1/', RecipeView.as_view(recipe_path = PATH.get('recipe1')), name='recipe-1'),
+    path('recipe/2/', RecipeView.as_view(recipe_path = PATH.get('recipe2')), name='recipe-2'),
 ]
--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -1,17 +1,18 @@
 import os, json
 from django.conf import settings
-
-from django.http import HttpResponse
 from django.shortcuts import redirect
 from django.views.generic import TemplateView
+
+PATH = {
+    'list': os.path.join(settings.BASE_DIR, 'static/data/recipe_list.json'),
+    'recipe1': os.path.join(settings.BASE_DIR, 'static/data/recipe_1.json'),
+    'recipe2': os.path.join(settings.BASE_DIR, 'static/data/recipe_2.json'),
+}
+
 
 def index(request):
     return redirect('recipes/list/')
 
-
-recipe_list_path = os.path.join(settings.BASE_DIR, 'static/data/recipe_list.json')
-recipe_1_path = os.path.join(settings.BASE_DIR, 'static/data/recipe_1.json')
-recipe_2_path = os.path.join(settings.BASE_DIR, 'static/data/recipe_2.json')
 
 def load_json_data(path):
     try:
@@ -22,38 +23,21 @@ def load_json_data(path):
         print(f"Error: JSON file not found at {path}")
         return None
 
+
 class RecipeListView(TemplateView):
     template_name = 'ledger/recipe_list.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        # json_file_path = os.path.join(settings.BASE_DIR, 'static/data/recipe_list_context.json')
-        
-        context['data'] = load_json_data(recipe_list_path)
-
+        context['data'] = load_json_data(PATH.get('list'))
         return context
-    
-class Recipe1View(TemplateView):
+
+
+class RecipeView(TemplateView):
     template_name = 'ledger/recipe.html'
+    recipe_path = PATH.get('recipe1')
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-
-        # TODO: Load recipe 1 data from JSON and add to context
-        context['data'] = load_json_data(recipe_1_path)
-
+        context['data'] = load_json_data(self.recipe_path)
         return context
-    
-class Recipe2View(TemplateView):
-    template_name = 'ledger/recipe.html'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-
-        # TODO: Load recipe 2 data from JSON and add to context
-        context['data'] = load_json_data(recipe_2_path)
-
-        return context
-    
-
-
