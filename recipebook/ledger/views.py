dc8515c1520e4fac98f20de93ef07cb61a7b4923
Zeke_ZB <zekiel.villasurda@gmail.com>
2026-03-02 16:47:11 +0800

    Create Profile Admin & Account Templates

--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import redirect
 from django.views.generic import TemplateView
 from .models import Recipe, RecipeIngredient
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 def index(request):
@@ -18,10 +19,11 @@ class RecipeListView(TemplateView):
         return context
 
 
-class RecipeDetailView(TemplateView):
+class RecipeDetailView(LoginRequiredMixin, TemplateView):
     model = Recipe
     template_name = 'ledger/recipe_detail.html'
     context_object_name = 'recipe_detail'
+    redirect_field_name = 'login'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
