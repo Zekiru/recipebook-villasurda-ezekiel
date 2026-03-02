bd23def7802dd83895839dd079fd81d207a361e0
Zeke_ZB <zekiel.villasurda@gmail.com>
2026-03-02 16:28:56 +0800

    Implement Profile Model

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -1,5 +1,18 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
+from django.core.validators import MinLengthValidator
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    short_bio = models.TextField(
+        validators=[MinLengthValidator(256)],
+    )
+
+    def __str__(self):
+        return self.name
 
 
 class Ingredient(models.Model):
@@ -18,6 +31,14 @@ class Ingredient(models.Model):
 
 class Recipe(models.Model):
     name = models.CharField(max_length=100)
+    author = models.ForeignKey(
+        Profile,
+        on_delete=models.CASCADE,
+        related_name='recipes',
+        default=None
+    )
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
