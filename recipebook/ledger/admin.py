3a7e784ac9e595961ddbd08d155bc5340d62556c
Zeke_ZB <zekiel.villasurda@gmail.com>
2026-03-12 21:45:18 +0800

    Create RecipeImage Inline for RecipeAdmin

--- recipebook/ledger/admin.py
+++ recipebook/ledger/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
 
-from .models import Profile, Recipe, RecipeIngredient
+from .models import Profile, Recipe, RecipeIngredient, RecipeImage
 
 
 class ProfileInline(admin.StackedInline):
@@ -18,13 +18,20 @@ class RecipeIngredientInline(admin.TabularInline):
     model = RecipeIngredient
 
 
+class RecipeImageInline(admin.TabularInline):
+    model = RecipeImage
+
+
 class RecipeAdmin(admin.ModelAdmin):
     model = Recipe
     search_fields = ['name']
     list_display = ['name']
-    inlines = [RecipeIngredientInline]
+    inlines = [
+        RecipeIngredientInline,
+        RecipeImageInline,
+    ]
 
 
-admin.site.register(Recipe, RecipeAdmin)
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
+admin.site.register(Recipe, RecipeAdmin)
